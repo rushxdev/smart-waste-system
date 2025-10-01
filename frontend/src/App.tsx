fec48dd2c5b9869eda7e383b128da108317a56a0
rushxdev <rashmikapabasara@gmail.com>
2025-10-01 22:05:38 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+      <div className="flex justify-center items-center h-screen bg-amber-600 text-white p-4 text-center text-2xl font-bold">
+        Smart Waste Management System
+      </div>
+  )
+}
+
+export default App
